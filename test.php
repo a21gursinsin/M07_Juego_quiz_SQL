b9d847e718a68f7458bd31e40cf9f151b2b010ef
a21gursinsin <a21gursinsin@inspedralbes.cat>
2022-10-06 11:49:19 +0200

    insertada las dades en phpmyadmin y quiz.json borrado

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "uzurdrive.ddns.net:3307";
+$username = "gur";
+$password = "1234";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
